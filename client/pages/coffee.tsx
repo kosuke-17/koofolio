44f275c5d4790836e00c364bdb6ed839976033aa
kosuke-17 <tamutamu17@icloud.com>
2022-07-01 08:22:49 +0900

    テーブル作成

--- client/pages/coffee.tsx
+++ client/pages/coffee.tsx
@@ -1,7 +1,94 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import React from "react";
 
+function createData(
+  name: string,
+  short: number,
+  tall: number,
+  grande: number,
+  venti: number
+) {
+  return { name, short, tall, grande, venti };
+}
+
 const Coffee = () => {
-  return <div>Coffee</div>;
+  const data = [
+    { name: "コーヒー", price: 500, size: "tall" },
+    { name: "コーヒー", price: 500, size: "tall" },
+    { name: "コーヒー", price: 500, size: "tall" },
+    { name: "コーヒー", price: 500, size: "tall" },
+    { name: "コーヒー", price: 500, size: "tall" },
+    { name: "コーヒー", price: 500, size: "tall" },
+  ];
+  const row = [
+    createData("ドリップ コーヒー", 280, 320, 360, 400),
+    createData("カフェ ミスト", 320, 360, 400, 440),
+    createData("スターバックスラテ", 330, 370, 410, 450),
+    createData("カプチーノ", 330, 370, 410, 450),
+    createData("カフェ アメリカーノ", 300, 340, 380, 420),
+    createData("キャラメル マキアート", 330, 370, 410, 450),
+    createData("カフェモカ/ホワイトモカ", 330, 370, 410, 450),
+  ];
+  const row2 = [
+    createData("ドリップ コーヒー", 280, 320, 360, 400),
+    createData("カフェ ミスト", 320, 360, 400, 440),
+    createData("スターバックスラテ", 330, 370, 410, 450),
+    createData("カプチーノ", 330, 370, 410, 450),
+    createData("カフェ アメリカーノ", 300, 340, 380, 420),
+    createData("キャラメル マキアート", 330, 370, 410, 450),
+    createData("カフェモカ/ホワイトモカ", 330, 370, 410, 450),
+  ];
+  const rows = [row, row2];
+
+  return (
+    <div>
+      {rows.map((rows) => {
+        return (
+          <TableContainer component={Paper}>
+            <Table
+              sx={{ minWidth: 650 }}
+              size="small"
+              aria-label="a dense table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
+                  <TableCell align="right">Short</TableCell>
+                  <TableCell align="right">Tall</TableCell>
+                  <TableCell align="right">Grande</TableCell>
+                  <TableCell align="right">Venti</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.short}</TableCell>
+                    <TableCell align="right">{row.tall}</TableCell>
+                    <TableCell align="right">{row.grande}</TableCell>
+                    <TableCell align="right">{row.venti}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Coffee;
